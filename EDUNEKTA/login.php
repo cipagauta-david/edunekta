c21ceace7ecad0154cf77df71bb22782f73e63c0
kevindiaz15 <polsion155@gmail.com>
2025-07-31 21:18:21 -0400

    login funcional

---
+++ EDUNEKTA/login.php
@@ -0,0 +1,32 @@
+<?php
+require_once './DEMO/Conexion.php'; 
+
+$conexionBD = new Conexion();
+$conn = $conexionBD->getConexion();
+
+if (!$conn) {
+    die("No se pudo establecer conexión con la base de datos.");
+}
+
+$correo = $_POST['correo'];
+$cedula = $_POST['cedula'];
+
+$correo = $conn->real_escape_string($correo);
+$cedula = $conn->real_escape_string($cedula);
+
+$sql = "SELECT * FROM personal WHERE correo = '$correo' AND cedula = '$cedula'";
+$resultado = $conn->query($sql);
+
+if ($resultado->num_rows > 0) {
+    session_start();
+    $_SESSION['correo'] = $correo;
+    header("Location: DEMO/dashboard.html"); 
+    exit();
+} else {
+    // Redirige con un parámetro de error
+    header("Location: login.html?error=1");
+    exit();
+}
+
+$conn->close();
+?>
